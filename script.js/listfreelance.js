77f9435e6815f1d0917d250883d9d9f020d796df
abbiiyu <abbiiyu>
2024-11-15 18:29:43 +0700

    add : profile dropdown

--- script.js/listfreelance.js
+++ script.js/listfreelance.js
@@ -6,3 +6,22 @@
  
 
  document.getElementById("profileIcon").textContent = initials;
+
+
+
+
+
+function toggleDropdown() {
+const dropdown = document.getElementById('dropdownMenu');
+dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
+}
+
+// Menutup dropdown jika klik di luar elemen
+document.addEventListener('click', function(event) {
+const profileIcon = document.getElementById('profileIcon');
+const dropdown = document.getElementById('dropdownMenu');
+
+if (!profileIcon.contains(event.target) && !dropdown.contains(event.target)) {
+    dropdown.style.display = 'none';
+}
+});
